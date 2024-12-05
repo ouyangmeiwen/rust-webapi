88e2e5ab5798dea28cd562c5c3c524696fe317c1
ouyangmeiwen <27672729+ouyangmeiwen@users.noreply.github.com>
2024-12-05 14:28:30 +0800

    123
    123

--- src/diesel_impl/infra.rs
+++ src/diesel_impl/infra.rs
@@ -2,17 +2,17 @@ use diesel;
 use diesel::r2d2::ConnectionManager;
 
 pub type Pool<T> = r2d2::Pool<ConnectionManager<T>>;
-pub type MySQLPool = Pool<diesel::mysql::MysqlConnection>;
+//pub type MySQLPool = Pool<diesel::mysql::MysqlConnection>;
 pub type SqlitePool = Pool<diesel::sqlite::SqliteConnection>;
 
-#[cfg(feature = "mysql")]
-pub type DBConn = MySQLPool;
+// #[cfg(feature = "mysql")]
+// pub type DBConn = MySQLPool;
 
 #[cfg(feature = "sqlite")]
 pub type DBConn = SqlitePool;
 
 pub fn db_pool() -> DBConn {
-    let database_url = std::env::var("DATABASE_URL").unwrap_or("/tmp/test_examples.db".to_string());
+    let database_url = std::env::var("DATABASE_URL").unwrap_or("test_examples.db".to_string());
     println!("Using Database {}", database_url);
     let manager = ConnectionManager::<diesel::sqlite::SqliteConnection>::new(database_url);
     Pool::builder()
